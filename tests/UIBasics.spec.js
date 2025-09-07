7bdf24518d8bc607ccd2950a08223c070a970b75
Rajesh Ramtere <rajesh.ramtere@gmail.com>
2025-09-07 23:50:54 +0530

    First Playwright Test

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -3,5 +3,11 @@ import { test, expect } from '@playwright/test';
 test('First Playwright Script', async ({page}) => {
     
     await page.goto("https://www.saucedemo.com/v1/");
+
+    const pageTitle = await page.title();
+    console.log("Page title is: "+pageTitle);
+
+    await expect(page).toHaveTitle("Swag Labs");
+
 });
 
